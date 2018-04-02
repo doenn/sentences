fbcd5532940fe3e768ad5efef779b40723ab3227
Lewis Sall <lewissall@hotmail.com>
2018-04-02 00:36:17 -0400

    Length matching cases implemented

--- src/main/java/sentences/Sentences.java
+++ src/main/java/sentences/Sentences.java
@@ -1,19 +1,29 @@
 package sentences;
 
+import java.lang.Number;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Sentences {
 
-	private int result;
+	private Map<String, Object> results;						// Parsed results returned in Map containing sentence Length and Longest Words
 
 	/**
 	* No-arg constructor. Initializes the private result member.
 	*
 	*/
 	public Sentences(){
-		result = 0;
-
+		results = new HashMap<String, Object>();
+		results.put("Length", Integer.valueOf(0));				// Initialize sentence length as 0
+		results.put("Longest Words", new ArrayList<String>());			// Longest Words ArrayList begins with size of 0
 	}
 
 	/**
@@ -22,19 +32,49 @@ public class Sentences {
 	* @param sentence a sentence String.
 	* @return the word count and longest word.
 	*/
-	public int parseSentence(String sentence){
+	public Map parseSentence(String sentence){
 			
-		String wordMatch = null;
 
 		Pattern wordPattern = Pattern.compile("\\w+");
 		Matcher wordMatcher = wordPattern.matcher(sentence);
-	
-		while (wordMatcher.find()){
-			wordMatch = wordMatcher.group();
-			System.out.println("Word: " + wordMatch);
-		}
 
-		return result;
+		List longestWords = (ArrayList<?>)results.get("Longest Words");
+		Integer wordCount = 0;							// Initialize as 0
+
+		while (wordMatcher.find()){
+			final String wordMatch = wordMatcher.group();
+			wordCount++;
+
+
+			boolean sameLengthExists = ((ArrayList<String>)longestWords).stream().filter((String priorMatch)->{
+				return wordMatch.length() == priorMatch.length();
+			}).anyMatch((String sameLengthPriorMatch)->{
+				return !wordMatch.equalsIgnoreCase(sameLengthPriorMatch);	// Do not include new match when it already exists
+			});
+
+			if (sameLengthExists){
+				longestWords.add(wordMatch);		// Append match to end of list
+			}
+
+			((ArrayList<String>)longestWords).removeIf((String priorMatch)->{		
+				return wordMatch.length() > priorMatch.length();		// Remove shorter-length words that were previously matched.
+			});
+
+
+			if (longestWords.size() == 0){
+				longestWords.add(wordMatch);
+			}
+			
+		}
+		Integer sentenceLength = (Integer)results.get("Length");
+		sentenceLength.valueOf(wordCount);
+
+		for (String longestWord : (ArrayList<String>)longestWords){
+			System.out.println("Longest word: " + longestWord);
+		}
+		System.out.println("Word count: " + wordCount);
+
+		return results;
 	}
 
 }
