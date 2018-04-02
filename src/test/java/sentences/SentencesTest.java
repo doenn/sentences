787e1bd37a2642fbd73b9ff1460febd46217f284
Lewis Sall <lewissall@hotmail.com>
2018-04-02 19:06:58 -0400

    Commas tests

--- src/test/java/sentences/SentencesTest.java
+++ src/test/java/sentences/SentencesTest.java
@@ -178,7 +178,7 @@ public class SentencesTest{
 	
 			@ParameterizedTest
 			@CsvFileSource(resources = "/Punctuation/Periods/Verify a sentence period is not included as the longest word.csv") 
-			@DisplayName ("Verify a sentence period is not included as the longest wordg")
+			@DisplayName ("Verify a sentence period is not included as the longest word")
 			@Tag ("Unit")
 			public void periodsNotIncludedTest(String sentence, int expectedLength, String longestWord){		
 
@@ -225,6 +225,237 @@ public class SentencesTest{
 
 				assertEquals(sentenceLength, Integer.valueOf(expectedLength));
 			}
+
+		}
+
+
+		@Nested
+		public class CommasEllipsesTests{
+	
+					
+			@DisplayName ("Verify words are matched even when they are followed by commas - Multiple Match")
+			@Tag ("Unit")
+			@Test
+			public void wordsMatchedWithCommasMultipleMatchTest(){		
+				String sentence = "I had cake, milk, soda, and tea.";
+
+				int expectedLength = 7;
+				
+				String longestWord = "cake milk soda";
+
+
+				Map<String, Object> results;
+
+
+				Pattern expectedWordPattern = Pattern.compile("([^\\s]+)(\\s)?");		// Tokenize by spaces
+				Matcher expectedWordMatcher = expectedWordPattern.matcher(longestWord);
+
+
+				List expectedLongestWords = new ArrayList<String>();
+
+				while (expectedWordMatcher.find()){		// Parse expectedLongestWords arguments into ArrayList
+					String expectedWordMatch = expectedWordMatcher.group(1);
+					expectedLongestWords.add(expectedWordMatch);
+				}
+	
+
+				Sentences sentences = new Sentences();
+				results = sentences.parseSentence(sentence);
+
+
+				Integer sentenceLength = (Integer)results.get("Length");
+				List longestWords = (ArrayList<?>)results.get("Longest Words");
+	
+
+				assertEquals(longestWords.size(), expectedLongestWords.size());
+
+				for (String expectedWord :  (ArrayList<String>)expectedLongestWords){
+
+					long sameWordCount = ((ArrayList<String>)longestWords).stream()
+								.filter((String returnedWord)->{
+									return expectedWord.equals(returnedWord);
+								})
+								.count();
+					assertEquals(sameWordCount, 1);					// Only one match, no more.
+				}
+		
+
+				assertEquals(sentenceLength, Integer.valueOf(expectedLength));
+			}
+
+			@DisplayName ("Verify words are matched even when they are followed by commas One Match")
+			@Tag ("Unit")
+			@Test
+			public void wordsMatchedWithCommasOneMatchTest(){		
+	
+				String sentence = "I had cake.";
+
+				int expectedLength = 3;
+				
+				String longestWord = "cake";
+
+
+
+
+				Map<String, Object> results;
+
+
+				Pattern expectedWordPattern = Pattern.compile("([^\\s]+)(\\s)?");		// Tokenize by spaces
+				Matcher expectedWordMatcher = expectedWordPattern.matcher(longestWord);
+
+
+				List expectedLongestWords = new ArrayList<String>();
+
+				while (expectedWordMatcher.find()){		// Parse expectedLongestWords arguments into ArrayList
+					String expectedWordMatch = expectedWordMatcher.group(1);
+					expectedLongestWords.add(expectedWordMatch);
+				}
+	
+
+				Sentences sentences = new Sentences();
+				results = sentences.parseSentence(sentence);
+
+
+				Integer sentenceLength = (Integer)results.get("Length");
+				List longestWords = (ArrayList<?>)results.get("Longest Words");
+	
+
+				assertEquals(longestWords.size(), expectedLongestWords.size());
+
+				for (String expectedWord :  (ArrayList<String>)expectedLongestWords){
+
+					long sameWordCount = ((ArrayList<String>)longestWords).stream()
+								.filter((String returnedWord)->{
+									return expectedWord.equals(returnedWord);
+								})
+								.count();
+					assertEquals(sameWordCount, 1);					// Only one match, no more.
+				}
+		
+
+				assertEquals(sentenceLength, Integer.valueOf(expectedLength));
+			}
+
+			@DisplayName ("Verify words are matched when followed by commas but the commas are not matched - Multiple match")
+			@Tag ("Unit")
+			@Test
+			public void commasNotIncludedMultipleMatchTest(){		
+
+
+
+				String sentence = "I had cake, milk, soda, and tea.";
+
+				int expectedLength = 7;
+				
+				String longestWord = "cake milk soda";
+
+
+
+
+				Map<String, Object> results;
+
+
+				Pattern expectedWordPattern = Pattern.compile("([^\\s]+)(\\s)?");		// Tokenize by spaces
+				Matcher expectedWordMatcher = expectedWordPattern.matcher(longestWord);
+
+
+				List expectedLongestWords = new ArrayList<String>();
+
+				while (expectedWordMatcher.find()){		// Parse expectedLongestWords arguments into ArrayList
+					String expectedWordMatch = expectedWordMatcher.group(1);
+					expectedLongestWords.add(expectedWordMatch);
+				}
+	
+
+				Sentences sentences = new Sentences();
+				results = sentences.parseSentence(sentence);
+
+
+				Integer sentenceLength = (Integer)results.get("Length");
+				List longestWords = (ArrayList<?>)results.get("Longest Words");
+				
+				String commaChar = ",";
+	
+				for (String actualLongestWord : (ArrayList<String>)longestWords){
+					assertNotEquals(actualLongestWord.contains(commaChar), true);		// Longest words do not contain periods
+				}
+
+				assertEquals(longestWords.size(), expectedLongestWords.size());
+
+				for (String expectedWord :  (ArrayList<String>)expectedLongestWords){
+
+					long sameWordCount = ((ArrayList<String>)longestWords).stream()
+								.filter((String returnedWord)->{
+									return expectedWord.equals(returnedWord);
+								})
+								.count();
+					assertEquals(sameWordCount, 1);					// Only one match, no more.
+				}
+		
+
+				assertEquals(sentenceLength, Integer.valueOf(expectedLength));
+			}
+
+			@DisplayName ("Verify words are matched when followed by commas but the commas are not matched - one match")
+			@Tag ("Unit")
+			@Test
+			public void commasNotIncludedOneMatchTest(){		
+
+
+				String sentence = "I had cake.";
+
+				int expectedLength = 3;
+				
+				String longestWord = "cake";
+
+
+
+
+
+				Map<String, Object> results;
+
+
+				Pattern expectedWordPattern = Pattern.compile("([^\\s]+)(\\s)?");		// Tokenize by spaces
+				Matcher expectedWordMatcher = expectedWordPattern.matcher(longestWord);
+
+
+				List expectedLongestWords = new ArrayList<String>();
+
+				while (expectedWordMatcher.find()){		// Parse expectedLongestWords arguments into ArrayList
+					String expectedWordMatch = expectedWordMatcher.group(1);
+					expectedLongestWords.add(expectedWordMatch);
+				}
+	
+
+				Sentences sentences = new Sentences();
+				results = sentences.parseSentence(sentence);
+
+
+				Integer sentenceLength = (Integer)results.get("Length");
+				List longestWords = (ArrayList<?>)results.get("Longest Words");
+				
+				String commaChar = ",";
+	
+				for (String actualLongestWord : (ArrayList<String>)longestWords){
+					assertNotEquals(actualLongestWord.contains(commaChar), true);		// Longest words do not contain periods
+				}
+
+				assertEquals(longestWords.size(), expectedLongestWords.size());
+
+				for (String expectedWord :  (ArrayList<String>)expectedLongestWords){
+
+					long sameWordCount = ((ArrayList<String>)longestWords).stream()
+								.filter((String returnedWord)->{
+									return expectedWord.equals(returnedWord);
+								})
+								.count();
+					assertEquals(sameWordCount, 1);					// Only one match, no more.
+				}
+		
+
+				assertEquals(sentenceLength, Integer.valueOf(expectedLength));
+			}
+
 
 		}
 
