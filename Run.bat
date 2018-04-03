89753f2be7d55729be99a2b4d70754f3b63dec14
Lewis Sall <lewissall@hotmail.com>
2018-04-03 05:43:16 -0400

    Custom executor script updates

--- Run.bat
+++ Run.bat
@@ -1,3 +1,3 @@
 cd src/main/java ^
- && java -cp sentences;. sentences.Main
+ && java -cp sentences;../../../junit-platform-console-standalone-1.1.0.jar;../../test/resources;../../test/java;. sentences.Main
 cd ../../../
