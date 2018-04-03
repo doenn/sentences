6cd94fe2d3c30310f3197da441878648bd21ceaf
Lewis Sall <lewissall@hotmail.com>
2018-04-03 05:43:27 -0400

    Custom executor script updates

--- Test.bat
+++ Test.bat
@@ -1,5 +1,6 @@
 cd src/test/java
-rmdir /S /Q report\junit
+rmdir /S /Q ..\..\..\report\junit
+rmdir /S /Q ..\..\..\report\allure
 java -jar ../../../junit-platform-console-standalone-1.1.0.jar --reports-dir ../../../report/junit --cp sentences;../../main/java;../resources;. --select-class sentences.SentencesTest
 cd ../../../
 mkdir report\allure
